35fc1a18c0d3837a446a22bec4cd63a3e5b31774
Christy Rollinson <crollinson@gmail.com>
2018-09-21 16:16:31 -0500

    Shells for npn functions to be written

---
+++ R/npn_getPlantTypes.R
@@ -0,0 +1,26 @@
+##' Align meteorology datasets for debiasing
+# -----------------------------------
+# Description
+# -----------------------------------
+##'
+##' @title 
+##' @family download NPN data
+##' @author Christy Rollinson
+##' @description 
+# -----------------------------------
+# Notes
+# -----------------------------------
+##' @details For more information on options, see: https://docs.google.com/document/d/1yNjupricKOAXn6tY1sI7-EwkcfwdGUZ7lxYv7fcPjO8/edit
+# -----------------------------------
+# Parameters
+# -----------------------------------
+##' @param 
+##' @export
+# -----------------------------------
+# Outputs
+# -----------------------------------
+##' @return 
+# -----------------------------------
+# Workflow
+# -----------------------------------
+# -----------------------------------
